bb97280bb3e011754b4fcd772ecf41ae50d8439b
tek-sys-hub <tekawasthi87@gmail.com>
2026-06-23 21:08:03 +0545

    Question From Leet Code

--- array-questions/array/src/Main.java
+++ array-questions/array/src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
                 for (int i = 0; i < nums.length; i++) {
                     for (int j = i + 1; j < nums.length; j++) {
-                        if (nums[i] + nums[2] == target) {
+                        if (nums[i] + nums[j] == target) {
                             return new int[] {i , j};
                         }
                     }
