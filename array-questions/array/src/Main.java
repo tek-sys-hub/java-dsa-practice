1eb182ba09d7940bec2d931d703f2ddfbbf45ef2
tek-sys-hub <tekawasthi87@gmail.com>
2026-06-23 21:05:01 +0545

    Question From Leet Code

--- array-questions/array/src/Main.java
+++ array-questions/array/src/Main.java
@@ -1,13 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+// https://leetcode.com/problems/two-sum/description/
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {5,6,4,7,1,2,3,6};
+        int target = 10;
+
+        int[] ans = twoSum(nums , target);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] twoSum (int[] nums , int target) {
+                if(nums.length == 0) {return new int[]{-1,-1};}
+
+                for (int i = 0; i < nums.length; i++) {
+                    for (int j = i + 1; j < nums.length; j++) {
+                        if (nums[i] + nums[2] == target) {
+                            return new int[] {i , j};
+                        }
+                    }
+                }
+        return new int[]  {0,0};
     }
 }
