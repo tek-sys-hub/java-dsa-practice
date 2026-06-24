a19a836acf50671a8efb357c5276c2b9fe9326b5
tek-sys-hub <tekawasthi87@gmail.com>
2026-06-24 22:04:00 +0545

    Binary Search

--- searching/BinarySearch/src/Main.java
+++ searching/BinarySearch/src/Main.java
@@ -3,3 +3,10 @@ public class Main {
 
     }
 }
+
+/*Procedure of binary search
+* step 1 : Find the middle value
+* step 2 : If the middle value == target terminate
+* step 3 : Compare the target with left and right side of middle values
+* step 4 : Now divide the remaning and again repeat the upper part
+* */
